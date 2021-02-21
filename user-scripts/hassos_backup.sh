4416d6d83a12167c9d1799ff8b8bc8e55189ac1c
Dustin <56093730+Dustin36@users.noreply.github.com>
2021-02-21 20:32:35 +0100

    some improvements

--- user-scripts/hassos_backup.sh
+++ user-scripts/hassos_backup.sh
@@ -11,12 +11,14 @@ BACKUPFILE="HassOS_Backup.zip"
 
 # ----- Do not touch! -----
 virsh shutdown $VMNAME
+sleep 30s
 rm $DIR/$BACKUPFILE
 rm $DIR/tmp -r
 mkdir $DIR/tmp
 cp $DIR/$VDISK $DIR/tmp/$VDISK
 virsh start $VMNAME
 /usr/local/emhttp/webGui/scripts/notify -e "Unraid Server Notice" -s "HassOS" -d "VM started!" -i "normal"
-zip $DIR/$BACKUPFILE $DIR/tmp/$VDISK 
+cd $DIR/tmp
+zip $DIR/$BACKUPFILE $VDISK
 /usr/local/emhttp/webGui/scripts/notify -e "Unraid Server Notice" -s "HassOS" -d "Backup finished!" -i "normal"
 rm $DIR/tmp -r
